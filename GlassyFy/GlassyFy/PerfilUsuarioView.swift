822cabdee97bb5d223c739f3879b549d855dd575
Aula11 <aula11@iMac-03.local>
2022-11-25 18:00:47 +0100

    Subida

--- GlassyFy/GlassyFy/PerfilUsuarioView.swift
+++ GlassyFy/GlassyFy/PerfilUsuarioView.swift
@@ -7,15 +7,11 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PerfilUsuarioView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
