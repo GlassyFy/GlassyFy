73801fcf917860e99e3ea0261d7eeb9b247c5687
Ángel Joaquín GARCÍA MÁRQUEZ <72364000+agarmar@users.noreply.github.com>
2022-12-09 10:09:29 +0100

    Update VistaEliminacionExperimento.swift
    Depurado

--- GlassyFy/Usuario/VistaEliminacionExperimento.swift
+++ GlassyFy/Usuario/VistaEliminacionExperimento.swift
@@ -24,40 +24,37 @@ struct VistaEliminacionExperimento: View {
     @State var cancelar = true 
     @Environment(\.presentationMode) var modoPresentacion
     let fecha1 = DateFormatter()
-    var body: some View {
-        
+    var body: some View {  
         Color(red: 48 / 255, green: 49 / 255, blue: 54 / 255)
-            .ignoresSafeArea()
-            .overlay(
+        .ignoresSafeArea()
+        .overlay(
         VStack {
-          Text("¿Seguro desea eliminar el experimento?")
+            Text("¿Seguro desea eliminar el experimento?")
                 .frame(width: 215, height: 47, alignment: .center)//355
                 .font(.custom("Arial", size:20))
                 .multilineTextAlignment(.center)
                 .offset(x:0, y:-30)
-            HStack{
-              Image(systemName: "books.vertical.fill")
+            HStack{ //Datos del experimento
+                Image(systemName: "books.vertical.fill")
                   .resizable()
                   .frame(width: 37, height: 31)
-                  .padding(5)  //spacing
+                  .padding(5)  
                   .background(colorBlue)
                   .cornerRadius(10)
-                  //.foregroundColor(colorBlue)
-                  //.offset(x:25, y:0)
 
-              VStack(alignment: .leading){
-                  Text(experimentoCurrent.nombre!)
-                      .frame(width: 305, alignment: .center)
-                      .font(.custom("Arial", size:20))
-                      .multilineTextAlignment(.center)
-                  Text("Fecha Creación: \(fecha1.string(from: experimentoCurrent.fechaCreacion!))")
-                      .frame(width: 305, alignment: .center)
-                      .multilineTextAlignment(.center)
-                      .font(.custom("Arial", size:14))
-              }
-              .frame(width: 305, alignment: .center)
-              .offset(x:10, y:0)
-              .multilineTextAlignment(.center)
+                VStack(alignment: .leading){
+                    Text(experimentoCurrent.nombre!)
+                        .frame(width: 305, alignment: .center)
+                        .font(.custom("Arial", size:20))
+                        .multilineTextAlignment(.center)
+                    Text("Fecha Creación: \(fecha1.string(from: experimentoCurrent.fechaCreacion!))")
+                        .frame(width: 305, alignment: .center)
+                        .multilineTextAlignment(.center)
+                        .font(.custom("Arial", size:14))
+                }
+                .frame(width: 305, alignment: .center)
+                .offset(x:10, y:0)
+                .multilineTextAlignment(.center)
             }
             .background(colorRect)
             .foregroundColor(.white)
@@ -65,65 +62,52 @@ struct VistaEliminacionExperimento: View {
             .offset(x:25, y:0)
             .overlay(RoundedRectangle(cornerRadius:10).stroke(colorStroke, lineWidth: 2))
             
-            HStack{
-              Button() {
-                //print ("action NO")
-                cancelar = true
-                modoPresentacion.wrappedValue.dismiss()
-              } label: {
-                  Text("No").bold()
-                      .frame(width: 176, height: 24, alignment: .center)
-                      .font(.custom("Arial", size:20)) //.font(.largeTitle)
-                      //.background(colorBTNNo)
-                      //.cornerRadius(2)
-              }
-              .frame(width: 130, height: 33, alignment: .center)
-              .padding()
-              .background(colorBTNNo)
-              .clipShape(Capsule())
+            HStack{  //Botones
+                Button() {
+                    cancelar = true
+                    modoPresentacion.wrappedValue.dismiss()
+                } label: {
+                    Text("No").bold()
+                        .frame(width: 176, height: 24, alignment: .center)
+                        .font(.custom("Arial", size:20)) 
+                }
+                .frame(width: 130, height: 33, alignment: .center)
+                .padding()
+                .background(colorBTNNo)
+                .clipShape(Capsule())
 
-              Spacer()
+                Spacer()
 
-              Button() {
-                //print ("action SI") //vm.deleteExperimento(experimento: experimentoCurrent)
-                cancelar = false
-                modoPresentacion.wrappedValue.dismiss()
-              } label: {
-                  Text("Sí").bold()
-                    .frame(width: 176, height: 24, alignment: .center)
-                    .font(.custom("Arial", size:20)) //.font(.largeTitle)
-                    //.background(colorBTNSi)
-                    //.cornerRadius(2)
-              }
-              .frame(width: 130, height: 33, alignment: .center)//170X53
-              .padding()
-              .background(colorBTNSi)
-              .clipShape(Capsule())
+                Button() {
+                    cancelar = false
+                    modoPresentacion.wrappedValue.dismiss()
+                } label: {
+                    Text("Sí").bold()
+                        .frame(width: 176, height: 24, alignment: .center)
+                        .font(.custom("Arial", size:20)) 
+                }
+                .frame(width: 130, height: 33, alignment: .center)//170X53
+                .padding()
+                .background(colorBTNSi)
+                .clipShape(Capsule())
             } //HStack
             .frame(width: 360, height: 53, alignment: .center) //400
             .offset(x:0, y:25)
             .font(.custom("Arial", size:20)) //.font(.largeTitle)
-            //.fontWeight(.bold)
-            //.foregroundColor(.white)
-          } //VStack
-          .frame (width: 425, height: 307, alignment: .center) //(width: 455, height: 307
-          .overlay(RoundedRectangle(cornerRadius:15).stroke(colorStroke, lineWidth: 2))
-          .background(colorRect)
-          .foregroundColor(.white)
+        } //VStack
+        .frame (width: 425, height: 307, alignment: .center) //(width: 455, height: 307
+        .overlay(RoundedRectangle(cornerRadius:15).stroke(colorStroke, lineWidth: 2))
+        .background(colorRect)
+        .foregroundColor(.white)
         )
     }
     
-  }
+}
 
-  /*struct VistaEliminacionExperimento_Previews: PreviewProvider {
+/*struct VistaEliminacionExperimento_Previews: PreviewProvider {
       @EnvironmentObject var vm: ViewModel
       static var previews: some View {
-          // List(ViewModel().datos){ // List(ModeloDatos().arrExperimentos){
-              // VistaFila(experimentoCurrent: $0)
-           //}
-          //List(ViewModel()){ // List(ModeloDatos().arrExperimentos){
-              VistaEliminacionExperimento(experimentoCurrent: vm.experimentosArray[0])
+          VistaEliminacionExperimento (experimentoCurrent: vm.experimentosArray[3])
               .environmentObject(vm)   //Falla
-          //}
        }
-   }*/
+}*/
