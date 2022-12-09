7d92c8db9c6c0ba1863fb502e3392c5f476f5e71
Ángel Joaquín GARCÍA MÁRQUEZ <72364000+agarmar@users.noreply.github.com>
2022-12-09 10:20:54 +0100

    Update VistaFilaHistoricoUsuario.swift
    Depurado

--- GlassyFy/Usuario/VistaFilaHistoricoUsuario.swift
+++ GlassyFy/Usuario/VistaFilaHistoricoUsuario.swift
@@ -1,11 +1,23 @@
 //
-//  VistaFilaExperimento.swift
+//  VistaFilaHistoricoUsuario.swift
 //  GlassyFy
 //
-//  Created by Aula11 on 25/11/22.
+//  Created by Aula11 on 18/11/22.
 //
 
 import SwiftUI
+
+/*
+var colorFondo: Color = Color(red: 48/255, green: 49/255, blue: 54/255)
+var colorRect: Color = Color(red: 56/255, green: 57/255, blue: 62/255)
+var colorcampostxt: Color = Color(red: 65 / 255, green: 68 / 255, blue: 74 / 255)
+var colorBlue: Color = Color(red: 73/255, green: 82/255, blue: 189/255)  //var colorSeleccion
+var colorGreen: Color = Color(red: 90/255, green: 163/255, blue: 100/255)
+var colorStroke: Color = Color(red: 101/255, green: 101/255, blue: 101/255)
+var colorLabel: Color = Color(red: 150/255, green: 152/255, blue: 157/255)
+var colorRojoTxt: Color = Color(red: 199/255, green: 73/255, blue: 69/255) //var colorSalir
+var colorRojoBoton: Color = Color(red: 237/255, green: 106/255, blue: 94/255) //var colorBTNSi
+*/
 
 struct VistaFilaHistoricoUsuario: View {
     @EnvironmentObject var vm: ViewModel
@@ -18,10 +30,8 @@ struct VistaFilaHistoricoUsuario: View {
         Color(red: 48 / 255, green: 49 / 255, blue: 54 / 255)
         .ignoresSafeArea()
         .overlay(
-            //NavigationView {
+        //NavigationView {
             HStack{
-                //NavigationView {
-                //Link (destination: VistaExperimentoUsuario(usuarioCurrent: vm.usuariosArray[0], experimentoCurrent: experimentoCurrent)) {
                 NavigationLink (destination: VistaExperimentoUsuario(usuarioCurrent: vm.usuariosArray[0], experimentoCurrent: experimentoCurrent)) {
                     Image(systemName: "books.vertical.fill")
                         .resizable()
@@ -30,53 +40,44 @@ struct VistaFilaHistoricoUsuario: View {
                         .background(colorBlue)
                         .cornerRadius(10)
                         .offset(x:10, y:0)
-                        //.onTapGesture {
-                            //NavigationLink(<#LocalizedStringKey#>, destination: VistaExperimentoUsuario(usuarioCurrent: vm.usuariosArray[0], experimentoCurrent: //experimentoCurrent))
-                        //}
                 }
-                //}
-                //.navigationBarBackButtonHidden(true)
                 VStack{
-                  Text(experimentoCurrent.nombre!)
-                    .font(.custom("Arial", size:20))
-                  //Text("Fecha Creación: DD/MM/YY") //(String(experimentoCurrent.fechaToma))// fecha)
+                    Text(experimentoCurrent.nombre!)
+                        .font(.custom("Arial", size:20))
                     Text("Fecha Creación: \(fecha1.string(from: experimentoCurrent.fechaCreacion!))")
-                    //Text("Fecha Creación: \(fecha1.string(from: experimentoCurrent.fechaCreacion!)!)")
-                    .font(.custom("Arial", size:14))
+                        .font(.custom("Arial", size:14))
                 }
                 .frame(width:189,  height:40, alignment: .leading)
                 .offset(x:5, y:0)
                 Spacer()
-                //Image(uiImage: UIImage(data: vm.usuariosArray[0].foto!)!)//("foto")
-                    //.frame(width:234, height:215)
+
                 Button() {
                     mostrarConfirmarEliminarExperimento.toggle()
-                    //VistaEliminacionExperimento (experimentoCurrent: experimentoCurrent)
                 } label: {
                     Image(systemName: "trash.circle.fill")
-                      .resizable()
-                      .background(colorFondo)
-                      .foregroundColor(colorRojoTxt)
-                      .clipShape(Circle())
-                      .frame(width: 40, height: 40)
-                      .offset(x:-10, y:0)
-                      .sheet (isPresented: $mostrarConfirmarEliminarExperimento,
-                        onDismiss: {
-                            if !cancelar {
-                                                vm.deleteExperimento(experimento: experimentoCurrent )
+                        .resizable()
+                        .background(colorFondo)
+                        .foregroundColor(colorRojoTxt)
+                        .clipShape(Circle())
+                        .frame(width: 40, height: 40)
+                        .offset(x:-10, y:0)
+                        .sheet (isPresented: $mostrarConfirmarEliminarExperimento,
+                            onDismiss: {
+                                if !cancelar {
+                                    vm.deleteExperimento(experimento: experimentoCurrent )
                                 }
-                        }, content: {
-                                            VistaEliminacionExperimento(experimentoCurrent: experimentoCurrent)
-                        }
-                      )
+                            }, content: {
+                                    VistaEliminacionExperimento(experimentoCurrent: experimentoCurrent)
+                            }
+                        )
                 }
             } //HStak
             .frame(width:389,  height:40, alignment: .center) // 439x65
             .overlay(RoundedRectangle(cornerRadius: 10).stroke(colorStroke, lineWidth: 1))
             .background(colorRect)
             .foregroundColor(.white)
-            //} //NavigationView
-            //.navigationBarBackButtonHidden(true)
+        //} //NavigationView
+        //.navigationBarBackButtonHidden(true)
         )
     }
 }
